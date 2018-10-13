7232fa7b3ecdb630f020d0f4d73b53f9e4044be4
Giannis Toumboglou <itoumb@yahoo.gr>
2018-10-13 16:11:42 +0300

    Update Email.java

--- Email.java
+++ Email.java
@@ -22,7 +22,7 @@ public class Email {
         this.password = randomPassword(defaultPasswordLength);
         
         // Combine elements to generate e-mail
-        email = firstName.toLowerCase()+"."+lastName.toLowerCase()+"@"+department+companySuffix;
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + companySuffix;
     }
     
     // Ask for the department
@@ -30,22 +30,22 @@ public class Email {
         System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
         Scanner in  = new Scanner(System.in);
         int depChoice = in.nextInt();
-        if (depChoice == 1){
+        if (depChoice == 1) {
         	return "sales.";
-        	}
-        else if (depChoice == 2){
+        }
+        else if (depChoice == 2) {
         	return "dev.";
-        	}
-        else if (depChoice == 3){
+        }
+        else if (depChoice == 3) {
         	return "acct.";
-        	}
+        }
         else {
         	return "";
-        	}
+        }
     }
     
     // Generate a random password
-    private String randomPassword(int length){
+    private String randomPassword(int length) {
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%";
         char[] password = new char[length];
         for (int i=0; i<length; i++){
@@ -56,27 +56,27 @@ public class Email {
     }
     
     // Set the mailbox capacity
-    public void setMailboxCapacity(int capacity){
+    public void setMailboxCapacity(int capacity) {
         this.mailboxCapacity = capacity;
     }
     
     // Get the mailbox capacity
-    public int getMaiboxCapacity(){
+    public int getMaiboxCapacity() {
         return mailboxCapacity;
     }
 
     // Get the password
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 
     // Change the password
-    public void changePassword(String password){
+    public void changePassword(String password) {
         this.password = password;
     }
 
     // Show the user's information
-    public String showInfo(){
+    public String showInfo() {
         return "\nDISPLAY NAME: " + firstName + " " + lastName +
             "\nCOMPANY E-MAIL: " + email +
             "\nPASSWORD: " + password +
